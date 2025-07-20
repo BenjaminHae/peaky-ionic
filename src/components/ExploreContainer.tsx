bc58e52c3dca23435525728c9b8af971bc3525ad
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-07-20 10:47:10 +0200

    implement first Peak View

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,123 @@
 import './ExploreContainer.css';
+import PeakView, {PeakViewRef} from './PeakView';
+import { TransformWrapper, TransformComponent, ReactZoomPanPinchRef, useTransformContext } from "react-zoom-pan-pinch";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 interface ContainerProps { }
 
+const compassHeading = (alpha, beta, gamma: number) => {
+
+  // Convert degrees to radians
+  var alphaRad = alpha * (Math.PI / 180);
+  var betaRad = beta * (Math.PI / 180);
+  var gammaRad = gamma * (Math.PI / 180);
+
+  // Calculate equation components
+  var cA = Math.cos(alphaRad);
+  var sA = Math.sin(alphaRad);
+  var cB = Math.cos(betaRad);
+  var sB = Math.sin(betaRad);
+  var cG = Math.cos(gammaRad);
+  var sG = Math.sin(gammaRad);
+
+  // Calculate A, B, C rotation components
+  var rA = - cA * sG - sA * sB * cG;
+  var rB = - sA * sG + cA * sB * cG;
+  var rC = - cB * cG;
+
+  // Calculate compass heading
+  var compassHeading = Math.atan(rA / rB);
+
+  // Convert from half unit circle to whole unit circle
+  if(rB < 0) {
+    compassHeading += Math.PI;
+  }else if(rA < 0) {
+    compassHeading += 2 * Math.PI;
+  }
+
+  // Convert radians to degrees
+  compassHeading *= 180 / Math.PI;
+
+  return compassHeading;
+}
+
+
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
+  const peakViewRef = useRef<PeakViewRef | null>(null);
+  const [orientationAllowed, setOrientationAllowed] = useState(false);
+  const [direction,setDirection] = useState(0);
+  const [directionsDisabled,setDirectionsDisabled] = useState(true);
+  const [positionX,setPositionX] = useState(0);
+  const [offsetX,setOffsetX] = useState(0);
+
+  const gotoDirection = (direction: number, fast: boolean = true) => {
+    peakViewRef.current?.zoomToDirection(direction, fast);
+    setDirection(direction);
+  }
+  
+  const orientationListener = useCallback((event) => {
+    gotoDirection(compassHeading(event.alpha, event.beta, event.gamma))
+  }, [setDirectionsDisabled]);
+
+  useEffect(() => {
+    if (!directionsDisabled) {
+      console.log("add event listener")
+      window.addEventListener("deviceorientation", orientationListener);
+    } else {
+      console.log("remove event listener")
+      window.removeEventListener("deviceorientation", orientationListener);
+    }
+    return () => window.removeEventListener("deviceorientation", orientationListener)
+}, [directionsDisabled, orientationListener])
+
+  const requestPermission = async () => {
+    try {
+      await DeviceMotionEvent.requestPermission();
+      setOrientationAllowed(true);
+      setDirectionsDisabled(false);
+    } catch {
+    }
+  }
+  if (!orientationAllowed && !DeviceMotionEvent.hasOwnProperty('requestPermission')) {
+    setOrientationAllowed(true);
+    setDirectionsDisabled(false);
+  }
+
+  const panningStart = (ref: ReactZoomPanPinchRef) => {
+    setDirectionsDisabled(true);
+    console.log(transformComponentRef.current);
+    console.log(` Panning start x: ${transformComponentRef.current?.instance.transformState.positionX} scale: ${transformComponentRef.current?.instance.transformState.scale}`);
+    setPositionX(transformComponentRef.current?.instance.transformState.positionX / transformComponentRef.current?.instance.transformState.scale);
+  }
+
+  const panningEnd = (ref: ReactZoomPanPinchRef) => {
+    setDirectionsDisabled(false);
+    console.log(` Panning end x: ${transformComponentRef.current?.instance.transformState.positionX} scale: ${transformComponentRef.current?.instance.transformState.scale}`);
+    const offset = positionX - transformComponentRef.current?.instance.transformState.positionX / transformComponentRef.current?.instance.transformState.scale;
+    peakViewRef.current?.writeOffset(offset);
+  }
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      { !orientationAllowed && <strong><button onClick={requestPermission}>Allow Orientation access</button></strong> }
+      <p>
+        {direction.toFixed(0)}° { directionsDisabled &&  "Movement deactivated "} Move to 
+        <a onClick={()=>gotoDirection(0, false)}>North</a>, 
+        <a onClick={()=>gotoDirection(90, false)}>East</a>, 
+        <a onClick={()=>gotoDirection(180, false)}>South</a>, 
+        <a onClick={()=>gotoDirection(270, false)}>West</a>
+      </p>
+      <TransformWrapper 
+          initialScale={2} 
+          ref={transformComponentRef} 
+          className="fullSize"
+          onPanningStart={panningStart} 
+          onPinchingStart={()=>{setDirectionsDisabled(true)}}
+          onPanningStop={panningEnd} 
+          onPinchingStop={()=>{setDirectionsDisabled(false)}}>
+        <PeakView transformer={transformComponentRef} ref={peakViewRef} />
+      </TransformWrapper>
     </div>
   );
 };
---
+++ src/components/PeakView.tsx
@@ -0,0 +1,58 @@
+import './PeakView.css';
+import { TransformComponent, ReactZoomPanPinchRef, useTransformContext } from "react-zoom-pan-pinch";
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Motion } from '@capacitor/motion';
+
+interface ContainerProps { 
+  transformer: ReactZoomPanPinchRef
+}
+export interface PeakViewRef {
+  zoomToDirection: (direction: number, fast?: boolean) => void;
+  writeOffset: (offset: number) => void;
+};
+
+const PeakView: React.FC<ContainerProps> = forwardRef<PeakViewRef, ContainerProps>((props, ref) => {
+  const [width,setWidth] = useState(0);
+  const [offset,setOffset] = useState(0);
+  const transformContext = useTransformContext();
+  const zoomToDirection = (dir: number, fast: boolean=true) => {
+    if (props.transformer.current) {
+      const { setTransform } = props.transformer.current;
+      const scale = transformContext.transformState.scale;
+      console.log(`zooming with offset ${offset}`);
+      setTransform(-dir/360 * width * scale - offset*scale,transformContext.transformState.positionY, scale, fast ? 5 : 300);
+    }
+  };
+  const writeOffset = (offset: number) => {
+    console.log(`writing offset ${offset}`);
+    setOffset(offset);
+  }
+  useImperativeHandle(ref, () => ({
+    zoomToDirection: (dir: number, fast: boolean=true) => {
+      zoomToDirection(dir, fast);
+    },
+    writeOffset: (offset: number) => {
+      writeOffset(offset);
+    }
+  }));
+
+  const onImgLoad = ({target: img}) => {
+    setWidth(img.offsetWidth);
+  };
+
+  return (
+        <TransformComponent>
+          <div className="fullSize">
+
+            <img className="peakImg"
+              src="St.Gallenkirch.png"
+              alt="Peaks"
+              onLoad={onImgLoad}
+            ></img>
+          </div>
+        </TransformComponent>
+  );
+});
+
+export default PeakView;
