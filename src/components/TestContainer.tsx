477b784e302ce16ee7607739e2ab2d8501aa6281
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-08-04 20:59:46 +0200

    add painting to canvas

--- src/components/TestContainer.tsx
+++ src/components/TestContainer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo, useRef } from "react";
 import SyncTileSet, { LatLng } from 'srtm-elevation-async';
 import SrtmStorage from '../capacitor_srtm_storage';
 import { Capacitor } from '@capacitor/core';
@@ -9,6 +9,7 @@ interface ContainerProps { }
 const TestContainer: React.FC<ContainerProps> = () => {
   const [height, setHeight] = useState<number|null>(null);
   const [text, setText] = useState<array<string>>([]);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const storage = new SrtmStorage();
   const location = [ 47.020156, 9.978416 ];
   const options = { };
@@ -16,18 +17,21 @@ const TestContainer: React.FC<ContainerProps> = () => {
     options.provider = '/cache/{lat}{lng}.SRTMGL3S.hgt.zip';
   }
 
-  const getHeight = async () => {
+  /*const getHeight = async () => {
     const ll = new LatLng(location[0], location[1])
     
     const tileset = new SyncTileSet(storage, ll, ll, options);
     await tileset.init();
     setHeight(tileset.getElevation(ll));
-  }
+  }*/
+  // todo implement as web worker
+  // https://www.linkedin.com/pulse/react-web-workers-offloading-heavy-computations-smoother-alex-lomia
   const getPeaks = async () => {
     const write = (outtext:string) => {
       setText((s:Array<string>)=> [...s, outtext]);
     }
     const gl = new GeoLocation(location[0], location[1]);
+    write(`starting peak calculation`);
     const time = [performance.now()];
     const peaky = new Peaky(storage, gl, options);
     await peaky.init();
@@ -45,9 +49,14 @@ const TestContainer: React.FC<ContainerProps> = () => {
     time.push(performance.now());
     write(`calculating peaks took ${time[3]-time[2]}`);
     write(`found ${peaky.peaks.length} peaks`);
+    if (canvasRef.current) {
+      canvasRef.current.height = max_height - min_height;
+      canvasRef.current.width = peaky.options.circle_precision * 10;
+      peaky.drawView(canvasRef.current);
+    }
   }
-  getHeight();
-  getPeaks();
+  //getHeight();
+  useMemo(()=>getPeaks(), [canvasRef]);
 
   const textItems = text.map((line) => 
     <p>{line}</p>
@@ -57,6 +66,7 @@ const TestContainer: React.FC<ContainerProps> = () => {
     <div id="container">
       { height && <p> { height }</p> }
       { textItems }
+      <canvas ref={canvasRef} />
     </div>
   );
 };
