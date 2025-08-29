f65471e43fbc382b395afa79221fcb62751d3e72
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-08-29 19:34:54 +0200

    set correct worker type

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+//import prebundleWorkers from "vite-plugin-prebundle-workers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy()/*,
+    prebundleWorkers({
+      include: "src/workers/peaky.ts"
+    })*/
   ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+  },
+  worker: {
+    format: 'es'
   }
 })
