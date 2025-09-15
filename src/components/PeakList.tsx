411f4857303cc0e8234a399d9dd39785c740c9ed
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-09-15 08:50:51 +0200

    allow sorting peaks by elevation

--- src/components/PeakList.tsx
+++ src/components/PeakList.tsx
@@ -1,4 +1,5 @@
 //import './PeakView.css';
+import { useState, useEffect, useCallback } from "react";
 import { type GeoLocation, projected_height, type PeakWithDistance } from '@benjaminhae/peaky';
 import { IonItem, IonLabel, IonList } from '@ionic/react';
 import { IonButton, IonButtons } from '@ionic/react';
@@ -11,11 +12,30 @@ interface PeakListProps {
 }
 
 const PeakList: React.FC<PeakListProps> = (props:PeakListProps) => {
-  const peakItems = props.peaks.map(
+  const [sortingMethod, setSortingMethod] = useState<'name'|'elevation'>('name');
+  const peakItems = props.peaks.sort((peakA, peakB: PeakWithDistance) => {
+      let criteriaA, criteriaB;
+      if (sortingMethod === 'name') {
+        criteriaA = peakA.name;
+        criteriaB = peakB.name;
+      }
+      else {
+        criteriaA = peakA.elevation;
+        criteriaB = peakB.elevation;
+      }
+      if (criteriaA < criteriaB) {
+        return -1;
+      }
+      if (criteriaA > criteriaB) {
+        return 1;
+      }
+      return 0;
+    }
+  ).map(
    (peak, index) =>
-           <IonItem key={`peak-item-${index}`}>
-             <IonLabel>{peak.name} ({peak.elevation.toFixed(0)} m)</IonLabel><IonButtons slot="start"><IonButton><IonIcon icon={mapOutline}></IonIcon></IonButton><IonButton><IonIcon icon={navigateCircleOutline}></IonIcon></IonButton></IonButtons>
-           </IonItem>
+       <IonItem key={`peak-item-${index}`}>
+         <IonLabel>{peak.name} ({peak.elevation.toFixed(0)} m)</IonLabel><IonButtons slot="start"><IonButton><IonIcon icon={mapOutline}></IonIcon></IonButton><IonButton><IonIcon icon={navigateCircleOutline}></IonIcon></IonButton></IonButtons>
+       </IonItem>
      );
 
   return (
