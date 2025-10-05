124e88009328b707490fd6d1f54a7707d9c45f4d
Benjamin Häublein <benjaminhaeublein@gmail.com>
2025-10-05 09:04:26 +0200

    change compass styling

--- src/components/PeakView.tsx
+++ src/components/PeakView.tsx
@@ -183,7 +183,19 @@ const PeakView: React.FC<ContainerProps> = forwardRef<PeakViewRef, ContainerProp
 
   const directions = ["N","O","S","W"].map((dir, index) => {
       return [-1,0,1].map((canvasId) => {
-        return (<div key={`key-${index}-${canvasId}`} style={{position:"absolute", bottom:0, left: 2*(canWidth/4 * index + canvasId * canWidth), height:"100%", width:"1px", borderLeft: "5px solid light-dark(grey, white)", fontSize: "5em"}}>
+        return (<div 
+                 key={`key-${index}-${canvasId}`} 
+                 style={{
+                     position:"absolute", 
+                     bottom:0, 
+                     left: 2*(canWidth/4 * index + canvasId * canWidth), 
+                     height:"100%", 
+                     width:"1px", 
+                     borderLeftWidth: `${dir==="N" || dir === "S" ? "10" : "5"}px`,
+                     borderLeftStyle: `solid`,
+                     borderLeftColor: `${dir==="N" ? "red" : "light-dark(grey, white)"}`, 
+                     fontSize: "5em"
+               }}>
                  <KeepScale>{dir}</KeepScale>
           </div>)
       });
