049fd05861492e778365efee83ef9f1fabb5f9b6
Benjamin Häublein <benjaminhaeublein@gmail.com>
2026-04-18 19:02:44 +0200

    allow showing peak labels when hidden

--- src/components/Silhouette/PeakLabel.tsx
+++ src/components/Silhouette/PeakLabel.tsx
@@ -1,4 +1,5 @@
 import './PeakLabel.css';
+import { useState } from 'react';
 
 interface PeakLabelProps { 
   name: string;
@@ -6,10 +7,13 @@ interface PeakLabelProps {
   showByDefault: boolean;
 }
 const PeakLabel: React.FC<PeakLabelProps> = (props:PeakLabelProps) => {
+  const [selected, setSelected] = useState<boolean>(false);
   return (
     <div className="labelContainer" style={{zIndex: props.elevation}}>
-      <div className="peakStroke">
-        <div className="peakLabel">{props.showByDefault && <>{props.name} - {props.elevation} m</>}</div>
+      <div className="peakStroke" onClick={()=>{setSelected((val)=>!val)}} >
+        {(props.showByDefault || selected) && 
+          <div className="peakLabel">{props.name} - {props.elevation} m</div>
+        }
       </div>
     </div>
   );
