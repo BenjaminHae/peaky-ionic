bcf43496c4a95f70aff45455650f4f957560a460
Benjamin Häublein <benjaminhaeublein@gmail.com>
2026-06-09 20:51:23 +0200

    show region

--- src/components/PeakList.tsx
+++ src/components/PeakList.tsx
@@ -1,7 +1,7 @@
 //import './PeakView.css';
 import { useState, useEffect, useCallback } from "react";
 import { type GeoLocation, projected_height, type PeakWithDistance } from '@benjaminhae/peaky';
-import { IonItem, IonLabel, IonList } from '@ionic/react';
+import { IonItem, IonLabel, IonList, IonItemGroup, IonItemDivider } from '@ionic/react';
 import { IonButton, IonButtons } from '@ionic/react';
 import { IonIcon } from '@ionic/react';
 import { mapOutline, navigateCircleOutline } from 'ionicons/icons';
@@ -12,9 +12,17 @@ interface PeakListProps {
   peak_selector: (peak: PeakWithDistance, display:'map'|'silhouette') => void;
 }
 
-const PeakList: React.FC<PeakListProps> = (props:PeakListProps) => {
-  const [sortingMethod, setSortingMethod] = useState<'name'|'elevation'>('name');
-  const peakItems = props.peaks.sort((peakA, peakB: PeakWithDistance) => {
+const peakItemFromPeak = (peak, index) =>
+       <IonItem key={`peak-item-${index}`}>
+         <IonLabel>{peak.name} {peak.elevation.toFixed(0)} m (Entfernung: {(peak.distance/1000).toFixed(1)} km)</IonLabel>
+         <IonButtons slot="start">
+           <IonButton onClick={()=>props.peak_selector(peak, 'map')}><IonIcon icon={mapOutline}></IonIcon></IonButton>
+           <IonButton onClick={()=>props.peak_selector(peak, 'silhouette')}><IonIcon icon={navigateCircleOutline}></IonIcon></IonButton>
+         </IonButtons>
+       </IonItem>
+
+const peakSorter = (sortingMethod:string) => {
+  return (peakA, peakB: PeakWithDistance) => {
       let criteriaA, criteriaB;
       if (sortingMethod === 'name') {
         criteriaA = peakA.name;
@@ -32,16 +40,21 @@ const PeakList: React.FC<PeakListProps> = (props:PeakListProps) => {
       }
       return 0;
     }
-  ).map(
-   (peak, index) =>
-       <IonItem key={`peak-item-${index}`}>
-         <IonLabel>{peak.name} {peak.elevation.toFixed(0)} m (Entfernung: {(peak.distance/1000).toFixed(1)} km)</IonLabel>
-         <IonButtons slot="start">
-           <IonButton onClick={()=>props.peak_selector(peak, 'map')}><IonIcon icon={mapOutline}></IonIcon></IonButton>
-           <IonButton onClick={()=>props.peak_selector(peak, 'silhouette')}><IonIcon icon={navigateCircleOutline}></IonIcon></IonButton>
-         </IonButtons>
-       </IonItem>
-     );
+  }
+
+const PeakList: React.FC<PeakListProps> = (props:PeakListProps) => {
+  const [sortingMethod, setSortingMethod] = useState<'name'|'elevation'>('name');
+  const regions = [...new Set(props.peaks.map(p=>p.region))]
+  console.log(regions)
+  const peakItems = regions.map( (r) => 
+      <IonItemGroup>
+        <IonItemDivider>
+          <IonLabel>{r}</IonLabel>
+        </IonItemDivider>
+	{props.peaks.filter(p=> p.region == r).sort(peakSorter(sortingMethod)).map(peakItemFromPeak)}
+      </IonItemGroup>
+  )
+  //const peakItems = props.peaks.sort(peakSorter).map(peakItemFromPeak);
 
   return (
       <IonList>
